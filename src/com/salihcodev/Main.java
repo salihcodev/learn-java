81b47c43fd22b0419b4cd4038d886a19c8dee794
Ahmed <ahmedsalihdev@gmail.com>
2021-09-25 20:46:44 +0200

    Learn constants.

--- src/com/salihcodev/Main.java
+++ src/com/salihcodev/Main.java
@@ -3,6 +3,8 @@ package com.salihcodev;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        // constants that can not be re-assigned again.
+        // won't be changed later.
+        final String BLABLA_CONSTANT = "constant value";
     }
 }
