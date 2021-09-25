b3737b1c70177b9a2808530264f437079a70de33
Ahmed <ahmedsalihdev@gmail.com>
2021-09-25 14:22:32 +0200

    Learn about strings.

--- src/com/salihcodev/Main.java
+++ src/com/salihcodev/Main.java
@@ -3,6 +3,13 @@ package com.salihcodev;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        String forTrimText = "   this is the text   ";
+        String text = "this is the text";
+
+        System.out.println(text.replace("t", "X")); // Xhis is Xhe XexX
+        System.out.println(text.length()); // 16
+        System.out.println(text.endsWith("xt")); // true
+        System.out.println(forTrimText.trim()); // this is the text
+        System.out.println(text); // Original `text` value always, coz String is immutable.
     }
 }
