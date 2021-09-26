101fff3a589f3e560b98dbe397cf01ac69dcfead
Ahmed <ahmedsalihdev@gmail.com>
2021-09-26 12:47:51 +0200

    Learn how to read user input.

--- src/com/salihcodev/Main.java
+++ src/com/salihcodev/Main.java
@@ -1,8 +1,18 @@
 package com.salihcodev;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        Scanner scanner = new Scanner(System.in);
+        float typedFloatCLI = scanner.nextFloat();
+        byte typedByteCLI = scanner.nextByte();
+        // and so on with another types.
+        // NOTE: only one scanner to use in the same place.
+
+        System.out.print("Your name:");
+        String typedLineCLI = scanner.nextLine().trim();
+        System.out.println(typedLineCLI);
     }
 }
