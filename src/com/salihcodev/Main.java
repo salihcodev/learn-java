e55c9b9b78797afff8660775c3d325defa6be863
Ahmed <ahmedsalihdev@gmail.com>
2021-09-26 12:30:35 +0200

    Learn how to deal with/formating numbers, removing '.attach*'

--- src/com/salihcodev/Main.java
+++ src/com/salihcodev/Main.java
@@ -1,8 +1,15 @@
 package com.salihcodev;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+        System.out.println(currencyInstance.format(1556548)); // $1,556,548.00
+
+        // you don't need to initialize NumberFormat in ever time you use it.
+        String percent = NumberFormat.getPercentInstance().format(0.35); // 35%
+        System.out.println(percent);
     }
 }
